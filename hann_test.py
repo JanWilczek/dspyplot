e0d30bf7d44d9cf9e4e0f590a167bfbb1b1d0ac5
Jan Wilczek <janekw@vp.pl>
2023-01-20 09:35:00 +0100

    Hann window test.

---
+++ hann_test.py
@@ -0,0 +1,16 @@
+from math import cos, pi
+
+import scipy.signal
+
+
+def main():
+    for window_size in [10, 11]:
+        scipy_window = scipy.signal.hann(window_size, False)
+        print(scipy_window)
+        self_calculated_window = [0.5 * (1 - cos(2 * pi * n / window_size))
+                                  for n in range(0, window_size)]
+        print(self_calculated_window)
+
+
+if __name__ == '__main__':
+    main()
