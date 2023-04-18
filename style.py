021742431b040e2daab4df7505b756c0e5a14d26
Jan Wilczek <janekw@vp.pl>
2023-04-18 08:26:44 +0200

    lesson 311

--- style.py
+++ style.py
@@ -12,5 +12,6 @@ tetradic_color_2 = '#eb00ef'
 tetradic_color_3 = '#efef00'
 triadic_color_1 = '#04ef00'
 window_color = '#0008ef'
+grey = '#7c7c7c'
 save_params = {'dpi': 300, 'bbox_inches': 'tight', 'transparent': True}
 img_file_suffix = '.png'
