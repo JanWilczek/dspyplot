66194be3aef5658a9efcc38f4f64d4201dae928b
Jan Wilczek <janekw@vp.pl>
2024-01-05 11:27:13 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dspyplot',
+    version='0.1',
+    packages=find_packages(),
+    description='A library for plotting digital signal processing data.'
+                ' Removes a lot of boilerplate from DSP code. '
+                'Focuses mostly on audio signals.',
+    author='Jan Wilczek',
+    author_email='jan.wilczek@thewolfsound.com',
+    url='https://github.com/JanWilczek/dspyplot',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
