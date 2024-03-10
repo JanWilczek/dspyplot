Separated plot_signal. Fixed loudness normalization in file preprocessing:

dspyplot/audiofile.py
@@ -2,13 +2,19 @@ from os import walk
 from pathlib import Path
 import soundfile as sf
 import numpy as np
-from .signals import normalize_for_listening, apply_fade
+from .signals import normalize_for_listening, apply_fade, normalize_to_peak
 import logging as logger
+import pyloudnorm as pyln
 
 
 def _preprocess(signal, sample_rate: float):
     signal = apply_fade(signal, fade_length=1000)
     signal = normalize_for_listening(signal, sample_rate)
+
+    if np.any(np.abs(signal) >= 1):
+        logger.warning(f'Audio signal would clip after normalization to -16 dB LUFS. Normalizing to -1 dB instead.')
+        signal = pyln.normalize.peak(signal, -1)
+
     return signal
 
 